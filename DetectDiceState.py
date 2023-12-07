929447f7f091064b481c5c2fa43efef5f5ded0d2
Stukeley <klinowskirafal@gmail.com>
2023-12-07 15:04:11 +0100

    Poprawiono preprocessing obrazów.

--- DetectDiceState.py
+++ DetectDiceState.py
@@ -1,6 +1,8 @@
 import cv2
 import pytesseract
 import fnmatch
+import platform
+import numpy as np
 
 
 # Funkcja zwraca 1, jeśli gracz wygrał w kości, 0 jeśli przegrał
@@ -8,11 +10,24 @@ def DetectDiceState(img):
     lost_text = "zaginiony"
     won_text = "wygrał"
 
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    if platform.system() == "Windows":
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    elif platform.system() == "Darwin":
+        pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+
     img_cropped = img[200:350, 150:700]
+
+    # Grayscale
     img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
 
+    # Threshold
     _, thresholded_image = cv2.threshold(img_gray, 164, 255, cv2.THRESH_BINARY)
+
+    # Morphological operations
+    kernel = np.ones((1, 1), np.uint8)
+    thresholded_image = cv2.erode(thresholded_image, kernel, iterations=1)
+    thresholded_image = cv2.dilate(thresholded_image, kernel, iterations=1)
+
     text = pytesseract.image_to_string(thresholded_image, lang="pol")
     text = str(text).lower().strip()
 
